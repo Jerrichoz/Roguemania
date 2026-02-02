127e522a112c26f21ab1601b96365df465265ec8
Maksim <maksim.scheierman@gmx.de>
2026-02-02 13:45:59 +0100

    Initial Unity project setup

---
+++ Assets/MaxGaming/FinalCharacterController/Scripts/PlayerState.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+namespace MaxGaming.FinalCharacterController
+{
+    public class PlayerState : MonoBehaviour
+    {
+        [field: SerializeField] public PlayerMovementState CurrentPlayerMovementState { get; private set; } = PlayerMovementState.Idling;
+
+        public void SetPlayerMovementState(PlayerMovementState playerMovementState)
+        {
+            CurrentPlayerMovementState = playerMovementState;
+        }
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        public bool InGroundedState()
+        {
+            return IsStateGroundedState(CurrentPlayerMovementState);
+        }
+        public bool IsStateGroundedState(PlayerMovementState playerMovementState)
+        {
+            return playerMovementState == PlayerMovementState.Idling ||
+            playerMovementState == PlayerMovementState.Walking ||
+            playerMovementState == PlayerMovementState.Running ||
+            playerMovementState == PlayerMovementState.Sprinting;
+        }
+    }
+    public enum PlayerMovementState
+    {
+
+        Idling = 0,
+        Walking = 1,
+        Running = 2,
+        Sprinting = 3,
+        Jumping = 4,
+        Falling = 5,
+        Strafing = 6,
+    }
+}
+
